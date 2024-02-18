5fa0326f0cb886336c12f04bed4f6ccc944a9184
shon1442 <160408527+shon1442@users.noreply.github.com>
2024-02-18 16:38:24 +0200

    Create pdf_points__gui.py

---
+++ pdf_points__gui.py
@@ -0,0 +1,31 @@
+import tkinter as tk
+from tkinter import filedialog
+import PyPDF2
+
+def count_points(pdf_file):
+    occurrences = 0
+    with open(pdf_file, 'rb') as file:
+        reader = PyPDF2.PdfFileReader(file)
+        num_pages = reader.numPages
+        for page_num in range(num_pages):
+            page = reader.getPage(page_num)
+            text = page.extractText()
+            occurrences += text.lower().count('.')
+    return occurrences
+
+def choose_pdf_file():
+    file_path = filedialog.askopenfilename(filetypes=[("PDF files", "*.pdf")])
+    if file_path:
+        points_count = count_points(file_path)
+        result_label.config(text=f"כמות הנקודות בקובץ ה-PDF היא: {points_count}", wraplength=300)
+
+root = tk.Tk()
+root.title("ספירת נקודות בקובץ PDF")
+
+choose_file_button = tk.Button(root, text="בחר קובץ PDF", command=choose_pdf_file)
+choose_file_button.pack(pady=20)
+
+result_label = tk.Label(root, text="", font=("Arial", 12), wraplength=300)
+result_label.pack()
+
+root.mainloop()
